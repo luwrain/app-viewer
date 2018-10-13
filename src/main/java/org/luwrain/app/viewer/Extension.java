3f597a26abf9e86dbdc31a850369025603638ee3
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2018-10-14 03:21:08 +0700

    The extension class

---
+++ src/main/java/org/luwrain/app/viewer/Extension.java
@@ -0,0 +1,68 @@
+/*
+   Copyright 2012-2018 Michael Pozhidaev <michael.pozhidaev@gmail.com>
+
+   This file is part of LUWRAIN.
+
+   LUWRAIN is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 3 of the License, or (at your option) any later version.
+
+   LUWRAIN is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+*/
+
+package org.luwrain.app.viewer;
+
+import java.util.*;
+import java.io.*;
+
+import org.luwrain.base.*;
+import org.luwrain.core.*;
+import org.luwrain.cpanel.*;
+
+public final class Extension extends org.luwrain.core.extensions.EmptyExtension
+{
+    @Override public Command[] getCommands(Luwrain luwrain)
+    {
+	return new Command[]{
+	    new Command(){
+		@Override public String getName()
+		{
+		    return "viewer";
+		}
+		@Override public void onCommand(Luwrain luwrain)
+		{
+		    luwrain.launchApp("viewer");
+		}
+	    }};
+    }
+
+    @Override public ExtensionObject[] getExtObjects(Luwrain luwrain)
+    {
+	return new ExtensionObject[]{
+	    
+	    new Shortcut() {
+		@Override public String getExtObjName()
+		{
+		    return "viewer";
+		}
+		@Override public Application[] prepareApp(String[] args)
+		{
+		    NullCheck.notNullItems(args, "args");
+		    if (args.length == 0)
+			return new Application[]{new App()};
+		    final List<Application> v = new LinkedList();
+		    for(String s: args)
+			v.add(new App(new File(s)));
+		    if (v.isEmpty())
+			return new Application[]{new App()};
+		    return v.toArray(new Application[v.size()]);
+		}
+	    },
+
+	};
+    }
+}
